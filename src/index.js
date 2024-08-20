0902757aa88aeb1a2b33e8d33f686b147ac07def
rumpapl <rumpapaul0201@gmail.com>
2024-08-20 10:52:41 +0600

    add gitignore file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const generateHeaderBlock = require("./helpers/header-block");
 
 // Example No. 1
-generateHeaderBlock("Example: SIMPLE CHAT APP -- using event-emitter");
+generateHeaderBlock("TASK: SIMPLE CHAT APP -- using event-emitter");
 require("./simple-chat-app/");
 
 // hands on testing emitter methods on docs
-generateHeaderBlock("Example: EventEmitter Built-in Supports");
+generateHeaderBlock("TASK: EventEmitter Built-in Supports");
 require("./event-emitter-docs");
